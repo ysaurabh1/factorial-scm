a7b330f051439c219e59d54aeb8f3b0f5faf567c
Saurav Yadav <84660197+ysaurabh1@users.noreply.github.com>
2025-04-22 09:49:59 +0530

    Version 1: Iterative factorial

---
+++ factorial.py
@@ -0,0 +1,12 @@
+# Version 1: Iterative Factorial
+def factorial(n):
+    result = 1
+    for i in range(2, n+1):
+        result *= i
+    return result
+
+print("Factorial of 5 is:", factorial(5))
+
+
+Version 1: Iterative factorial
+
